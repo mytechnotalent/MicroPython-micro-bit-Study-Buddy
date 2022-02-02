043c92ed2d758defafbcd51991ff7c7012a0e6e6
Kevin Thomas <kevin@mytechnotalent.com>
2022-02-02 12:14:25 -0500

    Update sb_template.py

--- sb_template.py
+++ sb_template.py
@@ -39,8 +39,9 @@ generic_teq_m = {
 SPEED = 95
 
 
-def bot_proc(ted, question):
-    """Bot proc function
+def bot(ted, question):
+    """
+    Function to handle bot
     
     Params:
         ted: dict
@@ -88,8 +89,9 @@ def bot_proc(ted, question):
     gc.collect()
         
         
-def quiz_f_proc(teq):
-    """Fill in the blank quiz proc function
+def quiz_f(teq):
+    """
+    Function to fill in the blank quiz
     
     Params:
         teq: dict
@@ -151,8 +153,9 @@ def quiz_f_proc(teq):
     gc.collect()
     
     
-def quiz_m_proc(teq):
-    """Multiple choice quiz proc function
+def quiz_m(teq):
+    """
+    Function to handle multiple choice quiz
     
     Params:
         teq: dict
