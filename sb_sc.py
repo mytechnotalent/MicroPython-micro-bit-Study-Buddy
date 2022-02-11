c3080860b96b6e8f7e34fe86d9d3ad03923baf5d
Kevin Thomas <kevin@mytechnotalent.com>
2022-02-10 19:43:46 -0500

    Update sb_sc.py

--- sb_sc.py
+++ sb_sc.py
@@ -210,16 +210,20 @@ def bot(ted, question):
     """
     # Init LED happy image 
     display.show(Image.HAPPY)
+
     # This is an advanced topic as well however this little function
     # cleans out the unnecessary global objects or variables on what
     # we call the heap area in memory
     gc.collect()
+
     # Init response object
     response = ''
+
     # We want to make sure that our dictionary database can 
     # find all values even if you use a capital letter
     # so we convert everything to lowercase 
     question = question.lower()
+
     # If you type something other than an empty string that means 
     # question has a value so the rest of the code will continue
     # on
@@ -230,7 +234,9 @@ def bot(ted, question):
         # match an entry in the dictionary database and if it does
         # put the value in the _response object
         response = [val for key, val in ted.items() if key in question]
+            
         gc.collect()
+
         # If our bot got a response from us then make sure
         # we trigger the speaking or suprised image so our bot
         # can open its mouth to talk and then have our bot
@@ -239,13 +245,19 @@ def bot(ted, question):
         # recognized then provide a custom default response
         if response:
             display.show(Image.SURPRISED)
+            
             print('BOT: {0}'.format(response[0]))
+            
             say(str(response[0]), speed=SPEED)
+            
             display.show(Image.HAPPY)
         else:
             display.show(Image.SURPRISED)
+            
             print('BOT: That is not something I am familiar with.')
+            
             say('That is not something I am familiar with.', speed=SPEED)
+            
             display.show(Image.HAPPY)
     gc.collect()
         
@@ -259,58 +271,87 @@ def quiz_f(teq):
     """
     # Init LED happy image 
     display.show(Image.HAPPY)
+
     # This is an advanced topic as well however this little function
     # cleans out the unnecessary global objects or variables on what
     # we call the heap area in memory
     gc.collect()
+
     # Init score object
     score = 0
+
     # Here we iterate through our quiz database
     for key in teq:
         print(key)
+
         say(str(key), speed=SPEED)
+            
         response = input('ANSWER: ')
         response = response.lower()
+
         correct_answer = teq[key].lower()
         if response == correct_answer:
             display.show(Image.SURPRISED)
+            
             print('CORRECT!')
+            
             say('CORRECT!', speed=SPEED)
+            
             display.show(Image.HAPPY)
             score += 1
         else:
             display.show(Image.SURPRISED)
+            
             print('The correct answer is {0}.'.format(teq[key]))
+            
             say('The correct answer is', speed=SPEED)
             say(str(teq[key]), speed=SPEED)
+            
             display.show(Image.HAPPY)
+            
         time.sleep(1)
+
         gc.collect()
+            
     # Here we reply to the student their score
     display.show(Image.SURPRISED)
+            
     print('You got {0} out of {1} correct!'.format(score, len(teq)))
+
     say('You got', speed=SPEED)
     say(str(score), speed=SPEED)
     say('out of', speed=SPEED)
     say(str(len(teq)), speed=SPEED)
     say('correct!', speed=SPEED)
+            
     # If student got a perfect score respond appropriately
     # or provide an encouring message to retry the quiz
     if score == len(teq):
         print('You got a perfect score!')
+
         say('You got a perfect score!', speed=SPEED)
+            
         print('Well done!')
+
         say('Well done!, ', speed=SPEED)
+            
         print('I am so proud of you!')
+
         say('I am so proud of you!', speed=SPEED)
+            
         play(POWER_UP)
     else:
         print('You are doing a great job!')
+
         say('You are doing a great job!', speed=SPEED)
+            
         print('I would LOVE for you to try again!')
+
         say('I would LOVE for you to try again!', speed=SPEED)
+            
     # Display happy response at the end of the quiz
     display.show(Image.HAPPY)
+            
     gc.collect()
     
     
@@ -323,37 +364,59 @@ def quiz_m(teq):
     """
     # Init LED happy image 
     display.show(Image.HAPPY)
+
     # This is an advanced topic as well however this little function
     # cleans out the unnecessary global objects or variables on what
     # we call the heap area in memory
     gc.collect()
+
     # Init score object
     score = 0
+
     # Here we iterate through our quiz database with multiple
     # choice items
     for key in teq:
         display.show(Image.SURPRISED)
+
         print(key)
+            
         say(str(key), speed=SPEED)
+
         display.show(Image.HAPPY)
+            
         time.sleep(1)
+
         display.show(Image.SURPRISED)
+            
         print('Press A for {0}.'.format(teq[key][0]))
+
         say('Press Ayyy for', speed=SPEED)
         say(str(teq[key][0]), speed=SPEED)
+
         display.show(Image.HAPPY)
+            
         time.sleep(1)
+
         display.show(Image.SURPRISED)
+            
         print('Touch the logo for {0}.'.format(teq[key][1]))
+
         say('Toch the logo for', speed=SPEED)
         say(str(teq[key][1]), speed=SPEED)
+
         display.show(Image.HAPPY)
+            
         time.sleep(1)
+
         display.show(Image.SURPRISED)
+            
         print('Press B for {0}.'.format(teq[key][2]))
+
         say('Press B for', speed=SPEED)
         say(str(teq[key][2]), speed=SPEED)
+
         display.show(Image.HAPPY)
+            
         while True:
             if button_a.is_pressed():
                 response = 0
@@ -366,54 +429,88 @@ def quiz_m(teq):
                 break
             else:
                 pass
+
         correct_answer = teq[key][3]
+            
         display.show(Image.SURPRISED)
+
         print('You selected {0}.'.format(teq[key][response]))
+            
         say('You selected', speed=SPEED)
         say(str(teq[key][response]), speed=SPEED)
+            
         display.show(Image.HAPPY)
+
         time.sleep(1)
+            
         if response == correct_answer:
             display.show(Image.SURPRISED)
+            
             print('CORRECT!')
+            
             say('CORRECT!', speed=SPEED)
+            
             display.show(Image.HAPPY)
             score += 1
         else:
             display.show(Image.SURPRISED)
+            
             print('The correct answer is {0}.'.format(teq[key][correct_answer]))
+            
             say('The correct answer is', speed=SPEED)
             say(str(teq[key][correct_answer]), speed=SPEED)
+            
             display.show(Image.HAPPY)
+            
         time.sleep(1)
+
         gc.collect()
+            
     # Here we reply to the student their score
     display.show(Image.SURPRISED)
+            
     print('You got {0} out of {1} correct!'.format(score, len(teq)))
+
     say('You got', speed=SPEED)
     say(str(score), speed=SPEED)
     say('out of', speed=SPEED)
     say(str(len(teq)), speed=SPEED)
     say('correct!', speed=SPEED)
+            
     display.show(Image.HAPPY)
+
     time.sleep(1)
+            
     # If student got a perfect score respond appropriately
     # or provide an encouring message to retry the quiz
     display.show(Image.SURPRISED)
+
     if score == len(teq):
         print('You got a perfect score!')
+
         say('You got a perfect score!', speed=SPEED)
+            
         print('Well done!')
+
         say('Well done!', speed=SPEED)
+            
         print('I am so proud of you!')
+
         say('I am so proud of you!', speed=SPEED)
+            
         play(POWER_UP)
     else:
         print('You are doing a great job!')
+
         say('You are doing a great job!', speed=SPEED)
+            
         print('I would LOVE for you to try again!')
+
         say('I would LOVE for you to try again!', speed=SPEED)
+            
     # Display happy response at the end of the quiz
     display.show(Image.HAPPY)
+            
     time.sleep(1)
+
     gc.collect()
